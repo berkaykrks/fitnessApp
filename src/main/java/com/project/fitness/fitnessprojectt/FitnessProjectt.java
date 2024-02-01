e8f539b24ce2abe21cde7aad62e95bb10980c192
faraz <faraz@DESKTOP-5PVKQ88>
2024-02-01 16:41:21 +0300

    MongoDB Bağlandı

--- src/main/java/com/project/fitness/fitnessprojectt/FitnessProjectt.java
+++ src/main/java/com/project/fitness/fitnessprojectt/FitnessProjectt.java
@@ -4,17 +4,25 @@
 
 package com.project.fitness.fitnessprojectt;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 /**
  *
  * @author faraz
  */
 public class FitnessProjectt {
-
-    public static void main(String[] args) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new SignUp().setVisible(true);
-            }
-        });
+    public static void main(String[] args) {        
+        MongoClient client = MongoClients.create("mongodb+srv://ata:12345@fitnessapp.ssm1bhf.mongodb.net/?retryWrites=true&w=majority");
+        
+        MongoDatabase db = client.getDatabase("berkay");
+        MongoCollection col = db.getCollection("berkayCollection");
+        Document sampleDoc=new Document("_id","1").append("name","John");
+        col.insertOne(sampleDoc);
+        
     }
+    
 }
---
+++ src/main/java/com/project/fitness/fitnessprojectt/MongoClientConnectionExample.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.project.fitness.fitnessprojectt;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+
+/**
+ *
+ * @author faraz
+ */
+public class MongoClientConnectionExample {
+    public static void main(String[] args) {
+        
+    }
+}
